400b01e4237b1010e6dc35fd17cb7581fd93e12d
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-07-27 13:54:01 +0530

    Added: Checking if the Last name is valid or not

---
+++ src/com/bridglabz/Regex.java
@@ -0,0 +1,41 @@
+package com.bridglabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+    static Scanner scanner = new Scanner(System.in);
+    static Pattern pattern;
+    static Matcher matcher;
+    //static String testString;
+
+    private static void validFirstName() {
+        pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
+        System.out.println("Enter First Name : ");
+        String firstName = scanner.nextLine();
+        matcher = pattern.matcher(firstName);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Not-Valid");
+        }
+    }
+
+    private static void validLastName() {
+        pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
+        System.out.println("Enter Last Name : ");
+        String lastName = scanner.nextLine();
+        matcher = pattern.matcher(lastName);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Not-Valid");
+        }
+    }
+
+    public static void main(String[] args) {
+        validFirstName();
+        validLastName();
+    }
+}
